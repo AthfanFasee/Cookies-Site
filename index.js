8bd92e7f5ed37e95a15ebc979df6c139b9e3dc48
Athfan <aththaar47@gmail.com>
2022-01-22 19:20:04 +0530

    All done!

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+// Creat Cookie
+const createCookies = () => {
+    const key = document.getElementById("createInputKey").value
+    const value = document.getElementById("createInputValue").value
+    const createDate = document.getElementById("createDate").value
+    
+    const y = Number(createDate.slice(0,4))
+    const m = Number(createDate.slice(5,7))-1
+    const d = Number(createDate.slice(8,10))  
+    const expiryDate = new Date(y, m, d).toUTCString()
+
+    document.cookie = `${key}=${value}; expires=${expiryDate}; path=/` 
+}
+
+// Update Cookie
+const updateCookies = () => {
+    const key = document.getElementById("updateInputKey").value
+    const value = document.getElementById("updateInputValue").value
+    const createDate = document.getElementById("updateDate").value
+    
+    const y = Number(createDate.slice(0,4))
+    const m = Number(createDate.slice(5,7))-1
+    const d = Number(createDate.slice(8,10))  
+    const expiryDate = new Date(y, m, d).toUTCString()
+
+    document.cookie = `${key}=${value}; expires=${expiryDate}; path=/` 
+}
+
+// Delete Cookie
+const deleteCookies = () => {
+    const key = document.getElementById("deleteInput").value
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+} 
+
+// Display Cookies
+const displayCookies = () => {
+    document.getElementById("display").innerText = document.cookie
+}
+
+const createButton = document.querySelector("#create")
+
+
+
+
+
+
+
+
+
+
